77356867416a867e7879d320f3af9813100dc7be
Karel Vervaeke <karel@ngdata.com>
2012-12-30 23:20:20 +0100

    Avoid 'rm' warnings about non-existant files

--- link.sh
+++ link.sh
@@ -12,7 +12,7 @@ function link() {
   (cd "$MYHOME"; find . -name .git -prune -o -type d -exec mkdir -p "$HOME"/{} ';')
 
   # remove old files that may be in the way
-  (cd "$MYHOME"; find . -type d -name .git -prune -o \! -name '*.swp' \! -name link.sh \! -name README.md -type f -exec rm "$HOME"/{} \;)
+  (cd "$MYHOME"; find . -type d -name .git -prune -o \! -name '*.swp' \! -name link.sh \! -name README.md -type f -exec rm -f "$HOME"/{} \;)
 
   # symlink files:
   (cd "$MYHOME"; find . -type d -name .git -prune -o \! -name '*.swp' \! -name link.sh \! -name README.md -type f -exec ln -sf "$MYHOME"/{} "$HOME"/{} \;)
