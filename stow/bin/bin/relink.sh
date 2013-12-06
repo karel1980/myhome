a75e1f5b7e85d98565607e7589624ea63ea7b80d
Karel Vervaeke <karel@ngdata.com>
2013-12-06 12:56:20 +0100

    relink.sh == stowit.sh

--- stow/bin/bin/relink.sh
+++ stow/bin/bin/relink.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-## Rerun the 'link.sh' script from 'myhome'
+# All stow modules
+if [ -z "$STOW_DATA" ]; then
+  echo "STOW_DATA is not set"
+fi
+cd "$STOW_DATA"
+stow -t /home/karel *
 
-~/work/personal/myhome/link.sh
+if [ -z "$STOW_DATA_EXTRA" ]; then
+  echo "STOW_DATA_EXTRA is not set"
+fi
+# secrets and other stuff I don't want in github are on Ubuntu One:
+cd "$STOW_DATA_EXTRA"
+stow -t /home/karel *
